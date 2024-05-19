7d8c41b03dc0841b61d29c24402e7a676bc1206f
Minz <94150757+Minzvx@users.noreply.github.com>
2024-05-19 19:49:30 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# APP
+# 仿饿了么DEMO
+
+![neleme](https://github.com/jdfdcc/vue-mobile-mint/blob/master/static/img/show.gif)
+
+> 框架 mint-ui
+
+```
+git clone https://github.com/jdfdcc/vue-mobile-mint.git
+
+cd vue-mobile-mint
+
+npm install 
+
+npm run dev
+
+```
+
+框架增加插件地址
+
+导航插件 https://github.com/zack24q/vue-navigation
+
+地址插件 https://github.com/jcc/v-distpicker
+
+
+```
+解决-webkit-overflow-scrolling引起的bug
+添加插件
+cordova plugin add https://github.com/ionic-team/cordova-plugin-wkwebview-engine.git --save
+github地址 https://github.com/ionic-team/cordova-plugin-wkwebview-engine
+npm run build --report
+```
+### -webkit-overflow-scrolling 
+-webkit-overflow-scrolling ：touch 当为局部滚动（如给某个指定的div设置滚动）时候才需要设置，全局滚动无需设置（body滚动）
+
+github地址 https://github.com/ionic-team/cordova-plugin-wkwebview-engine
+
+IOSApp下载地址：https://www.pgyer.com/FmvM
+
+突然好想重写一遍咋办......
+
+欢迎inssues 欢迎star
+
+群号:642855626 欢迎大家进群一起讨论关于前端的知识，一起讨论一起成长!!!
+
+
---
+++ build/webpack.base.conf.js
@@ -0,0 +1,60 @@
+var path = require('path')
+var utils = require('./utils')
+var config = require('../config')
+var vueLoaderConfig = require('./vue-loader.conf')
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  entry: {
+    app: './src/main.js'
+  },
+  output: {
+    path: config.build.assetsRoot,
+    filename: '[name].js',
+    publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+			'@': resolve('src'),
+			'swiper': 'swiper/dist/js/swiper.js',
+			'swiper-css': path.resolve(__dirname, '../node_modules/swiper/dist/css/')
+    }
+  },
+  module: {
+    loaders: [{
+      test: /\.css$/,
+      loader: "style-loader!css-loader!postcss-loader"
+    }],
+    rules: [{
+      test: /muse-ui.src.*?js$/,
+      loader: 'babel-loader'
+    }, {
+      test: /\.vue$/,
+      loader: 'vue-loader',
+      options: vueLoaderConfig
+    }, {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      include: [resolve('src'), resolve('test')]
+    }, {
+      test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+        name: utils.assetsPath('img/[name].[ext]')
+      }
+    }, {
+      test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+        name: utils.assetsPath('fonts/[name].[ext]')
+      }
+    }]
+  }
+}
